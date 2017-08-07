14bc858a188029bfdc968de389eb89004a7448eb
sarah19-meet <sarah19@meet.mit.edu>
2017-08-07 08:20:24 +0200

    Stopping here

--- RandomMaze.py
+++ RandomMaze.py
@@ -1,333 +1,184 @@
-import random
-import numpy as np
-from matplotlib import pyplot as plt
-import matplotlib.cm as cm
-
-num_rows = int(input("Rows: ")) # number of rows
-num_cols = int(input("Columns: ")) # number of columns
-
-# The array M is going to hold the array information for each cell.
-# The first four coordinates tell if walls exist on those sides 
-# and the fifth indicates if the cell has been visited in the search.
-# M(LEFT, UP, RIGHT, DOWN, CHECK_IF_VISITED)
-M = np.zeros((num_rows,num_cols,5), dtype=np.uint8)
-
-# The array image is going to be the output image to display
-image = np.zeros((num_rows*10,num_cols*10), dtype=np.uint8)
-
-# Set starting row and column
-r = 0
-c = 0
-history = [(r,c)] # The history is the stack of visited locations
-
-# Trace a path though the cells of the maze and open walls along the path.
-# We do this with a while loop, repeating the loop until there is no history, 
-# which would mean we backtracked to the initial start.
-while history: 
-    M[r,c,4] = 1 # designate this location as visited
-    # check if the adjacent cells are valid for moving to
-    check = []
-    if c > 0 and M[r,c-1,4] == 0:
-        check.append('L')  
-    if r > 0 and M[r-1,c,4] == 0:
-        check.append('U')
-    if c < num_cols-1 and M[r,c+1,4] == 0:
-        check.append('R')
-    if r < num_rows-1 and M[r+1,c,4] == 0:
-        check.append('D')    
-    
-    if len(check): # If there is a valid cell to move to.
-        # Mark the walls between cells as open if we move
-        history.append([r,c])
-        move_direction = random.choice(check)
-        if move_direction == 'L':
-            M[r,c,0] = 1
-            c = c-1
-            M[r,c,2] = 1
-        if move_direction == 'U':
-            M[r,c,1] = 1
-            r = r-1
-            M[r,c,3] = 1
-        if move_direction == 'R':
-            M[r,c,2] = 1
-            c = c+1
-            M[r,c,0] = 1
-        if move_direction == 'D':
-            M[r,c,3] = 1
-            r = r+1
-            M[r,c,1] = 1
-    else: # If there are no valid cells to move to.
-	# retrace one step back in history if no move is possible
-        r,c = history.pop()
-    
-         
-# Open the walls at the start and finish
-M[0,0,0] = 1
-M[num_rows-1,num_cols-1,2] = 1
-    
-# Generate the image for display
-for row in range(0,num_rows):
-    for col in range(0,num_cols):
-        cell_data = M[row,col]
-        for i in range(10*row+1,10*row+9):
-            image[i,range(10*col+1,10*col+9)] = 255
-            if cell_data[0] == 1: image[range(10*row+1,10*row+9),10*col] = 255
-            if cell_data[1] == 1: image[10*row,range(10*col+1,10*col+9)] = 255
-            if cell_data[2] == 1: image[range(10*row+1,10*row+9),10*col+9] = 255
-            if cell_data[3] == 1: image[10*row+9,range(10*col+1,10*col+9)] = 255
-
-# Display the image
-plt.imshow(image, cmap = cm.Greys_r, interpolation='none')
-plt.show()
-
-#Another example implementation [clarification needed] in Python/NumPy.
-
-#This algorithm works by creating n (density) islands of length p (complexity). An island is created by choosing a random starting point with odd coordinates, then a random direction is chosen. If the cell two steps in the direction is free, then a wall is added at both one step and two steps in this direction. The process is iterated for n steps for this island. p islands are created. n and p are expressed as float to apapt them to the size of the maze. With a low complexity, islands are very small and the maze is easy to solve. With low density, the maze has more "big empty rooms".
-
-import numpy
-from numpy.random import random_integers as rand
-import matplotlib.pyplot as pyplot
-
-def maze(width=81, height=51, complexity=.75, density=.75):
-    # Only odd shapes
-    shape = ((height // 2) * 2 + 1, (width // 2) * 2 + 1)
-    # Adjust complexity and density relative to maze size
-    complexity = int(complexity * (5 * (shape[0] + shape[1])))
-    density    = int(density * ((shape[0] // 2) * (shape[1] // 2)))
-    # Build actual maze
-    Z = numpy.zeros(shape, dtype=bool)
-    # Fill borders
-    Z[0, :] = Z[-1, :] = 1
-    Z[:, 0] = Z[:, -1] = 1
-    # Make aisles
-    for i in range(density):
-        x, y = rand(0, shape[1] // 2) * 2, rand(0, shape[0] // 2) * 2
-        Z[y, x] = 1
-        for j in range(complexity):
-            neighbours = []
-            if x > 1:             neighbours.append((y, x - 2))
-            if x < shape[1] - 2:  neighbours.append((y, x + 2))
-            if y > 1:             neighbours.append((y - 2, x))
-            if y < shape[0] - 2:  neighbours.append((y + 2, x))
-            if len(neighbours):
-                y_,x_ = neighbours[rand(0, len(neighbours) - 1)]
-                if Z[y_, x_] == 0:
-                    Z[y_, x_] = 1
-                    Z[y_ + (y - y_) // 2, x_ + (x - x_) // 2] = 1
-                    x, y = x_, y_
-    return Z
-
-pyplot.figure(figsize=(10, 5))
-pyplot.imshow(maze(80, 40), cmap=pyplot.cm.binary, interpolation='nearest')
-pyplot.xticks([]), pyplot.yticks([])
-pyplot.show()
-C code example[edit]
-The code below is an example of depth-first search maze generator in C.
-
-//Code by Jacek Wieczorek
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
-
-typedef struct
-{
-	int x, y; //Node position - little waste of memory, but it allows faster generation
-	void *parent; //Pointer to parent node
-	char c; //Character to be displayed
-	char dirs; //Directions that stil haven't been explored
-} Node;
-
-Node *nodes; //Nodes array
-int width, height; //Maze dimensions
-
-
-int init( )
-{
-	int i, j;
-	Node *n;
-	
-	//Allocate memory for maze
-	nodes = calloc( width * height, sizeof( Node ) );
-	if ( nodes == NULL ) return 1;
-		
-	//Setup crucial nodes
-	for ( i = 0; i < width; i++ )
-	{
-		for ( j = 0; j < height; j++ )
-		{
-			n = nodes + i + j * width;
-			if ( i * j % 2 ) 
-			{
-				n->x = i;
-				n->y = j;
-				n->dirs = 15; //Assume that all directions can be explored (4 youngest bits set)
-				n->c = ' '; 
-			}
-			else n->c = '#'; //Add walls between nodes
-		}
-	}
-	return 0;
+BEGIN [
+    # The maze dimensions.
+    width = 20;  # Global
+    height = 20; # Global
+    resetMaze();
+ 
+    # Some constants.
+    top = 1;
+    bottom = 2;
+    left = 3;
+    right = 4;
+ 
+    # Randomize the PRNG.
+    randomize();
+ 
+    # Visit all the cells starting at a random point.
+    visitCell(getRandX(), getRandY());
+ 
+    # Show the result.
+    printMaze();
+}
+ 
+# Wander through the maze removing walls as we go.
+function visitCell(x, y,    dirList, dir, nx, ny, ndir, pi) {
+    setVisited(x, y);   # This cell has been visited.
+ 
+    # Visit neighbors in a random order.
+    dirList = getRandDirList();
+    for (dir = 1; dir <= 4; dir++) {
+        # Get coordinates of a random neighbor (next in random direction list).
+        ndir = substr(dirList, dir, 1);
+        nx = getNextX(x, ndir);
+        ny = getNextY(y, ndir);
+ 
+        # Visit an unvisited neighbor, removing the separating walls.
+        if (wasVisited(nx, ny) == 0) {
+            rmWall(x, y, ndir);
+            rmWall(nx, ny, getOppositeDir(ndir));
+            visitCell(nx, ny)
+        } 
+    }
+}
+ 
+# Display the text-mode maze.
+function printMaze(    x, y, r, w) {
+    for (y = 1; y <= height; y++) {
+        for (pass = 1; pass <= 2; pass++) { # Go over each row twice: top, middle
+            for (x = 1; x <= width; x++) {
+                if (pass == 1) { # top
+                    printf("+");
+                    printf(hasWall(x, y, top) == 1 ? "---" : "   ");
+                    if (x == width) printf("+");
+                }
+                else if (pass == 2) { # left, right
+                    printf(hasWall(x, y, left) == 1 ? "|" : " ");
+                    printf("   ");
+                    if (x == width) printf(hasWall(x, y, right) == 1 ? "|" : " ");
+                }
+            }
+            print;
+        }
+    }
+    for (x = 1; x <= width; x++) printf("+---"); # bottom row
+    print("+"); # bottom right corner
+}
+ 
+# Given a direction, get its opposite.
+function getOppositeDir(d) {
+    if (d == top) return bottom;
+    if (d == bottom) return top;
+    if (d == left) return right;
+    if (d == right) return left;
+}
+ 
+# Build a list (string) of the four directions in random order.
+function getRandDirList(    dirList, randDir, nx, ny, idx) {
+    dirList = "";
+    while (length(dirList) < 4) {
+        randDir = getRandDir();
+        if (!index(dirList, randDir)) {
+            dirList = dirList randDir;
+        }
+    }
+    return dirList;
+}
+ 
+# Get x coordinate of the neighbor in a given a direction.
+function getNextX(x, dir) {
+    if (dir == left) x = x - 1;
+    if (dir == right) x = x + 1;
+    if (!isGoodXY(x, 1)) return -1; # Off the edge.
+    return x;
+}
+ 
+# Get y coordinate of the neighbor in a given a direction.
+function getNextY(y, dir) {
+    if (dir == top) y = y - 1;
+    if (dir == bottom) y = y + 1;
+    if (!isGoodXY(1, y)) return -1; # Off the edge.
+    return y;
+}
+ 
+# Mark a cell as visited.
+function setVisited(x, y,    cell) {
+    cell = getCell(x, y);
+    if (cell == -1) return;
+    cell = substr(cell, 1, 4) "1"; # walls plus visited
+    setCell(x, y, cell);
+}
+ 
+# Get the visited state of a cell.
+function wasVisited(x, y,    cell) {
+    cell = getCell(x, y);
+    if (cell == -1) return 1; # Off edges already visited.
+    return substr(getCell(x,y), 5, 1);
+}
+ 
+# Remove a cell's wall in a given direction.
+function rmWall(x, y, d,    i, oldCell, newCell) {
+    oldCell = getCell(x, y);
+    if (oldCell == -1) return;
+    newCell = "";
+    for (i = 1; i <= 4; i++) { # Ugly as concat of two substrings and a constant?.
+        newCell = newCell (i == d ? "0" : substr(oldCell, i, 1));
+    }
+    newCell = newCell wasVisited(x, y);
+    setCell(x, y, newCell);
+}
+ 
+# Determine if a cell has a wall in a given direction.
+function hasWall(x, y, d,    cell) {
+    cell = getCell(x, y);
+    if (cell == -1) return 1; # Cells off edge always have all walls.
+    return substr(getCell(x, y), d, 1);
+}
+ 
+# Plunk a cell into the maze.
+function setCell(x, y, cell,    idx) {
+    if (!isGoodXY(x, y)) return;
+    maze[x, y] = cell
+}
+ 
+# Get a cell from the maze.
+function getCell(x, y,    idx) {
+    if (!isGoodXY(x, y)) return -1; # Bad cell marker.
+    return maze[x, y];
+}
+ 
+# Are the given coordinates in the maze?
+function isGoodXY(x, y) {
+    if (x < 1 || x > width) return 0;
+    if (y < 1 || y > height) return 0;
+    return 1;
+}
+ 
+# Build the empty maze.
+function resetMaze(    x, y) {
+    delete maze;
+    for (y = 1; y <= height; y++) {
+        for (x = 1; x <= width; x++) {
+            maze[x, y] = "11110"; # walls (up, down, left, right) and visited state.
+        }
+    }
+}
+ 
+# Random things properly scaled.
+ 
+function getRandX() {
+    return 1 + int(rand() * width);
+}
+ 
+function getRandY() {
+    return 1 +int(rand() * height);
+}
+ 
+function getRandDir() {
+    return 1 + int(rand() * 4);
+}
+ 
+function randomize() {
+    "echo $RANDOM" | getline t;
+    srand(t);
 }
 
-Node *link( Node *n )
-{
-	//Connects node to random neighbor (if possible) and returns
-	//address of next node that should be visited
-
-	int x, y;
-	char dir;
-	Node *dest;
-	
-	//Nothing can be done if null pointer is given - return
-	if ( n == NULL ) return NULL;
-	
-	//While there are directions still unexplored
-	while ( n->dirs )
-	{
-		//Randomly pick one direction
-		dir = ( 1 << ( rand( ) % 4 ) );
-		
-		//If it has already been explored - try again
-		if ( ~n->dirs & dir ) continue;
-		
-		//Mark direction as explored
-		n->dirs &= ~dir;
-		
-		//Depending on chosen direction
-		switch ( dir )
-		{
-			//Check if it's possible to go right
-			case 1:
-				if ( n->x + 2 < width )
-				{
-					x = n->x + 2;
-					y = n->y;
-				}
-				else continue;
-				break;
-			
-			//Check if it's possible to go down
-			case 2:
-				if ( n->y + 2 < height )
-				{
-					x = n->x;
-					y = n->y + 2;
-				}
-				else continue;
-				break;
-			
-			//Check if it's possible to go left	
-			case 4:
-				if ( n->x - 2 >= 0 )
-				{
-					x = n->x - 2;
-					y = n->y;
-				}
-				else continue;
-				break;
-			
-			//Check if it's possible to go up
-			case 8:
-				if ( n->y - 2 >= 0 )
-				{
-					x = n->x;
-					y = n->y - 2;
-				}
-				else continue;
-				break;
-		}
-		
-		//Get destination node into pointer (makes things a tiny bit faster)
-		dest = nodes + x + y * width;
-		
-		//Make sure that destination node is not a wall
-		if ( dest->c == ' ' )
-		{
-			//If destination is a linked node already - abort
-			if ( dest->parent != NULL ) continue;
-			
-			//Otherwise, adopt node
-			dest->parent = n;
-			
-			//Remove wall between nodes
-			nodes[n->x + ( x - n->x ) / 2 + ( n->y + ( y - n->y ) / 2 ) * width].c = ' ';
-			
-			//Return address of the child node
-			return dest;
-		}
-	}
-	
-	//If nothing more can be done here - return parent's address
-	return n->parent;
-}
-
-void draw( )
-{
-	int i, j;
-
-	//Outputs maze to terminal - nothing special
-	for ( i = 0; i < height; i++ )
-	{
-		for ( j = 0; j < width; j++ )
-		{
-			printf( "%c", nodes[j + i * width].c );
-		}
-		printf( "\n" );
-	}
-}
-
-int main( int argc, char **argv )
-{
-	int i, badarg;
-	long seed;
-	Node *start, *last;
-
-	//Check argument count
-	if ( argc < 3 )
-	{
-		fprintf( stderr, "%s: please specify maze dimensions!\n", argv[0] );
-		exit( 1 );
-	}
-	
-	//Read maze dimensions from command line arguments
-	if ( sscanf( argv[1], "%d", &width ) + sscanf( argv[2], "%d", &height ) < 2 )
-	{
-		fprintf( stderr, "%s: invalid maze size value!\n", argv[0] );
-		exit( 1 );
-	}
-
-	//Allow only odd dimensions
-	if ( !( width % 2 ) || !( height % 2 ) )
-	{
-		fprintf( stderr, "%s: dimensions must be odd!\n", argv[0] );
-		exit( 1 );
-	}
-	
-	//Do not allow negative dimensions
-	if ( width <= 0 || height <= 0 )
-	{
-		fprintf( stderr, "%s: dimensions must be greater than 0!\n", argv[0] );
-		exit( 1 );
-	}
-
-	//Seed random generator
-	srand( time( NULL ) );
-	
-	//Initialize maze
-	if ( init( ) )
-	{
-		fprintf( stderr, "%s: out of memory!\n", argv[0] );
-		exit( 1 );
-	}
-	
-	//Setup start node
-	start = nodes + 1 + width;
-	start->parent = start;
-	last = start;
-	
-	//Connect nodes until start node is reached and can't be left
-	while ( ( last = link( last ) ) != start );
-	draw( );
-}
