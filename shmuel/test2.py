cec33a6ab491db6411f72f1397c8a227b4e3a2ce
sarah19-meet <sarah19@meet.mit.edu>
2017-08-09 17:31:45 +0200

    Stopping here

--- shmuel/test2.py
+++ shmuel/test2.py
@@ -52,28 +52,24 @@ RIGHT_EDGE=SIZE_X/2
 LEFT_EDGE=SIZE_X/-2
 
 def up():
-    if direction !=DOWN:
-        global direction
-        direction=UP
-        print('you pressed the up key!')
+    global direction
+    direction=UP
+    print('you pressed the up key!')
 
 def left():
-    if direction !=RIGHT:
-        global direction
-        direction=LEFT
-        print('you pressed the left key!')
+    global direction
+    direction=LEFT
+    print('you pressed the left key!')
 
 def down():
-    if direction !=UP:
-        global direction
-        direction=DOWN
-        print('you pressed the down key!')
+    global direction
+    direction=DOWN
+    print('you pressed the down key!')
 
 def right():
-    if direction !=LEFT:
-        global direction
-        direction=RIGHT
-        print('you pressed the right key!')
+    global direction
+    direction=RIGHT
+    print('you pressed the right key!')
     
 
 turtle.onkeypress(up,W_KEY)
@@ -104,23 +100,25 @@ def move_clinton():
     elif direction==DOWN:
         clinton.goto(x_pos,y_pos-SQUARE_SIZE)
         print('you moved down!')
+
+    
     turtle.ontimer(move_clinton,100)
 
     
 move_clinton()
 
 food_pos=[(100,100)]
-
+food_stamps=[]
 
 turtle.penup()
 stamp_list=[]
 food_pos=[]
 food_list=[]
 turtle.setup(SIZE_X,SIZE_Y)
-x_pos=snake.pos()[0] #Get x-position with snake.pos()[0]
-y_pos=snake.pos()[1]
+x_pos=clinton.pos()[0] #Get x-position with snake.pos()[0]
+y_pos=clinton.pos()[1]
 pos_list = []
-if snake.pos() in food_pos:
+if clinton.pos() in food_pos:
         food_ind=food_pos.index(snake.pos())
         food.clear_stamp(food_stamps[food_ind])
 min_x=-int(SIZE_X/3/SQUARE_SIZE)+1
@@ -128,12 +126,23 @@ max_x=int(SIZE_X/3/SQUARE_SIZE)-1
 min_y=-int(SIZE_Y/3/SQUARE_SIZE)-1
 max_y=int(SIZE_Y/3/SQUARE_SIZE)+1
 
+
+turtle.register_shape('hamburger.gif')
+food=turtle.clone()
+food.shape('hamburger.gif')
+food.goto(-100,100)
+
 def make_food():
-    turtle.register_shape('hamburger.gif')
-    food=turtle.clone()
-    food.shape('hamburger.gif')
-    food.goto(-100,100)
-   
+    global food_stamps, food_pos
+    if clinton.pos() in food_pos:
+        print(food_stamps)
+        food_ind=food_pos.index(clinton.pos())
+        food.clearstamp(food_stamps[food_ind])
+        food_pos.pop(food_ind)
+        food_stamps.pop(food_ind)
+        print('you have eaten the food!')
+        score1=score1+1
+        write(score1)   
     min_x=-int(SIZE_X/3/SQUARE_SIZE)+1
     max_x=int(SIZE_X/3/SQUARE_SIZE)-1
     min_y=-int(SIZE_Y/3/SQUARE_SIZE)-1
